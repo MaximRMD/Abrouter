f9939fb4aa698bb142a2e33cf1b036895ab40f18
Maxim <maxRMD@yandex.ru>
2022-04-28 20:22:59 +0300

    dev_page_experments

---
+++ src/js/smoothscroll.js
@@ -0,0 +1,18 @@
+const links = document.querySelectorAll('a')
+links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault()
+        const section = document.querySelector(link.getAttribute('href'))
+        if (section) {
+            // section.scrollIntoView({
+            //     block: 'start',
+            //     behavior: 'smooth'
+            // })
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "start",
+                inline: "center",
+            });
+        }
+    })
+})
